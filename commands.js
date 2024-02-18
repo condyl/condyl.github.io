3dfdc412595b4f2e7a182e4b11990b2a585e3b52
condyl <condyl678@gmail.com>
2024-02-18 09:52:55 -0500

    initial commit

---
+++ commands.js
@@ -0,0 +1,70 @@
+const linkedin = "https://www.linkedin.com/in/connor-bernard/"
+const github = "https://www.github.com/condyl";
+const email = "mailto:connorbbrocku@gmail.com";
+const resume = "https://drive.google.com/drive/folders/1oMk7rSnh0zGLh5Ctn3uV_Itk1NlV95ul?usp=sharing";
+
+const commands = [
+    "help",
+    "aboutme",
+    "experience",
+    "noncs",
+    "projects",
+    "linkedin",
+    "github",
+    "resume",
+    "email",
+    "clear"
+];
+
+const help = [
+    '<li>Press <kbd>Tab</kbd> to auto complete commands, and use <i class="fa-solid fa-arrow-up"></i> and <i class="fa-solid fa-arrow-down"></i> to see previous commands.</li>',
+    "<br>",
+    '<li>aboutme      - Learn a bit about me <i class="fa-solid fa-user-tie"></i></li>',
+    '<li>experience   - Check out my computer science related work history <i class="fa-solid fa-briefcase"></i></li>',
+    '<li>projects     - Check out my recent projects <i class="fa-solid fa-bars-progress"></i></li>',
+    '<li>noncs        - Look at my non-computer science related work history <i class="fa-solid fa-shop"></i></li>',
+    '<li>linkedin     - Check out my LinkedIn <i class="fa-brands fa-linkedin"></i></li>',
+    '<li>github       - Check out my GitHub profile <i class="fa-brands fa-github"></i></li>',
+    '<li>resume       - Look at my resume <i class="fa-solid fa-file"></i></li>',
+    '<li>email        - Send me an email <i class="fa-solid fa-envelope"></i></li>',
+    '<li>clear        - Clear the terminal <i class="fa-solid fa-trash"></i></li>',
+]
+
+const aboutme = [
+    "Hi!  I'm Connor!  I am currently a 2nd-year student at Brock University in St Catherines, Ontario studying Computer Science.  For the past couple months, I have been a Full Stack Developer Intern at Caribou Contests, where I have been the lead developer of a new puzzle-game that will be added to the site very soon.  My main CS interests are web development, computer vision, and artificial intelligence.  I am currently looking for a Fall 2024 co-op/internship."
+]
+
+const experience = [
+    "<br>",
+    `<li>Caribou Contests - Full Stack Developer Intern - Jan. 2024 to Apr. 2024</li>`,
+    "<br>"
+]
+
+const noncs = [
+    "<br>",
+    `<li>Fortinos Supermarkets - Front End Assistant - Aug. 2019 to present`,
+    `<li>Ford Motor Company - Summer Student - June 2023 to Aug. 2023</li>`,
+    "<br>"
+]
+
+const projects = [
+    "<br>",
+    `<li><a href="https://github.com/condyl/condyl.github.io" target="__blank"><u>Terminal Portfolio</u> <i class="fa-solid fa-link"></i> </a> - This Website!</li>`,
+    `<li><a href="https://github.com/condyl/wordle-solver" target="__blank"><u>Wordle Solver</u> <i class="fa-solid fa-link"></i></a> - Automatically solve Wordle puzzles!</li>`,
+    `<li><a href="https://github.com/condyl/recipe-discord-bot" target="__blank"><u>Resume Discord Bot</u> <i class="fa-solid fa-link"></i> </a> - Get step-by-step recipe instructions!</li>`,
+    `<li><a href="https://github.com/condyl/led-strip" target="__blank"><u>Website Controlled LED RGB Strip</u> <i class="fa-solid fa-link"></i> </a> - Control your lights on any device with a web browser!</li>`,
+    "<br>"
+];
+
+export {
+    linkedin,
+    github,
+    email,
+    commands,
+    experience,
+    noncs,
+    projects,
+    aboutme,
+    help,
+    resume
+};
---
+++ main.js
@@ -0,0 +1,164 @@
+import {
+    linkedin,
+    github,
+    resume,
+    email,
+    help,
+    aboutme,
+    commands,
+    projects,
+    experience,
+    noncs,
+} from "./commands.js";
+
+const container = document.querySelector(".container");
+const input = document.getElementById("prompt");
+
+let previousCommands = [];
+let index = 1;
+
+window.addEventListener("keydown", enterHandler);
+window.addEventListener("keydown", upArrowHandler);
+window.addEventListener("keydown", downArrowHandler);
+window.addEventListener("keydown", tabHandler);
+
+
+function enterHandler(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const command = input.value;
+        previousCommands.push(command);
+        if (command.toLowerCase() === "clear") {
+            previousCommands.push(command);
+            runCommand(command, null);
+            input.value = "";
+            scrollToCurrentInput;
+            return;
+        }
+
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("output");
+        newDiv.innerHTML = `
+    <div class="flexbox">
+        <span class="green nospace">guest</span>
+        <span class="light-dark nospace">@</span>
+        <span class="purple nospace">LINKHERE.COM</span>
+        <span class="light-dark nospace">:$~ </span>
+        <span class="space">${command}</span>
+    </div>
+    <div class="cmd-line"></div>
+    `;
+        const newOutput = newDiv.querySelector(".cmd-line");
+        runCommand(command, newOutput);
+        container.insertBefore(newDiv, container.lastElementChild);
+        input.value = "";
+        scrollToCurrentInput();
+    }
+}
+
+function upArrowHandler(event) {
+    if (event.key === "ArrowUp" && previousCommands.length !== 0) {
+        event.preventDefault();
+        input.value = previousCommands[previousCommands.length - index];
+        if (!(index + 1 > previousCommands.length)) {
+            index++;
+        }
+    }
+}
+
+function downArrowHandler(event) {
+    if (event.key === "ArrowDown" && previousCommands.length !== 0) {
+        event.preventDefault();
+        input.value = previousCommands[previousCommands.length - index];
+        if (index - 1 !== 0) {
+            index--;
+        }
+    }
+}
+
+function tabHandler(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      const incompleteWord = input.value.toLowerCase();
+      const match = commands.filter((command) =>
+        command.startsWith(incompleteWord),
+      );
+      if (match.length > 0) {
+        input.value = match[0];
+      }
+    }
+  }
+
+function runCommand(command, output) {
+    switch (command.toLowerCase()) {
+        case "help":
+            newText(help, output);
+            break;
+        case "aboutme":
+            newText(aboutme, output);
+            break;
+        case "experience":
+            newText(experience, output);
+            break;
+        case "noncs":
+            newText(noncs, output);
+            break;
+        case "projects":
+            newText(projects, output);
+            break;
+        case "linkedin":
+            newText("Opening LinkedIn...", output);
+            newLink(linkedin);
+            break;
+        case "github":
+            newText("Opening GitHub...", output);
+            newLink(github);
+            break;
+        case "resume":
+            newText("Opening resume...", output);
+            newLink(resume);
+            break;
+        case "email":
+            newText(`Opening mailto: <a href="${email}">connorbbrocku@gmail.com</a>...`, output);
+            break;
+        case "clear":
+            clearContent();
+            break;
+        case "":
+            newText("", output);
+            break;
+        default:
+            newText(`${command} : The term '${command}' is not recognized as the name of a cmdlet, function, script file, or operable program. Type 'help' to see the list of available commands.`, output);
+            break;
+    }
+}
+
+function newText(text, output) {
+    if (typeof text === "string") {
+        output.innerHTML = text;
+        return;
+    }
+    for (let x of text) {
+        output.innerHTML += x;
+    }
+}
+
+function newLink(link) {
+    scrollToCurrentInput();
+    setTimeout(function () {
+        window.open(link, "_blank");
+    }, 500);
+}
+
+function clearContent() {
+    const contentList = container.querySelectorAll(".output");
+    contentList.forEach((element) => {
+        element.remove();
+    });
+    scrollToCurrentInput();
+}
+
+function scrollToCurrentInput() {
+    window.scrollTo(0, document.body.offsetHeight);
+    input.focus();
+}
